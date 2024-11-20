9d03ff5ece5908bea1a7385ea2baf3b5dccb9f9a
WinyiGalindo <dr8147239@gmail.com>
2024-11-19 21:50:05 -0500

    Creacion Proyecto

---
+++ src/main/java/com/mycompany/proyectofinal/ProyectoFinal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectofinal;
+
+/**
+ *
+ * @author Winyi Galindo
+ */
+public class ProyectoFinal {
+
+    public static void main(String[] args) {
+        System.out.println("Con toda pal proyecto");
+    }
+}
