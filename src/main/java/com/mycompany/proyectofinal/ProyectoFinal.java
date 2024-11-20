60b194d0a58778fdad43c2002513b6a49873a19d
Santiago Soto <sotodavid405@gmail.com>
2024-11-19 22:05:24 -0500

    Prueba 1 push: 2 prints

--- src/main/java/com/mycompany/proyectofinal/ProyectoFinal.java
+++ src/main/java/com/mycompany/proyectofinal/ProyectoFinal.java
@@ -1,16 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.proyectofinal;
 
-/**
- *
- * @author Winyi Galindo
- */
 public class ProyectoFinal {
 
     public static void main(String[] args) {
         System.out.println("Con toda pal proyecto");
+        System.out.println("Full programadores");
+        System.out.println("pool?");
     }
 }
