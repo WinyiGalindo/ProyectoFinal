45b54d78238a2610b83b6ed946385f54f9343add
Santiago Soto <sotodavid405@gmail.com>
2024-12-08 18:29:44 -0500

    Pantallas

--- src/main/java/com/mycompany/proyectofinal/ProyectoFinal.java
+++ src/main/java/com/mycompany/proyectofinal/ProyectoFinal.java
@@ -1,10 +1,12 @@
 package com.mycompany.proyectofinal;
 
+import ProyectoFinal.vista.PantallaPrincipal;
 import java.util.Scanner;
 
 public class ProyectoFinal {
 
     public static void main(String[] args) {
-
+        PantallaPrincipal interfaz = new PantallaPrincipal();
+        interfaz.setVisible(true);
     }
 }
