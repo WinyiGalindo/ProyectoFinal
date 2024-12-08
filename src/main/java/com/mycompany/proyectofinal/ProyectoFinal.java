25827a3755070ae120f15ca2c344e5d38d46f0d7
Santiago Soto <sotodavid405@gmail.com>
2024-12-08 16:27:46 -0500

    Borra de cosas innecesarias

--- src/main/java/com/mycompany/proyectofinal/ProyectoFinal.java
+++ src/main/java/com/mycompany/proyectofinal/ProyectoFinal.java
@@ -1,13 +1,10 @@
 package com.mycompany.proyectofinal;
 
-import ProyectoFinal.vista.Menu;
 import java.util.Scanner;
 
 public class ProyectoFinal {
 
     public static void main(String[] args) {
-        
-        Scanner input = new Scanner (System.in);
-        Menu.mostrar_menu();
+
     }
 }
