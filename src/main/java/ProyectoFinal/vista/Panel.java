ffbcc506ed9461228d511dae63bb0ec5e88d1fb5
Santiago Soto <sotodavid405@gmail.com>
2024-12-08 16:36:37 -0500

    Borra de cosas innecesarias

--- src/main/java/ProyectoFinal/vista/Panel.java
+++ src/main/java/ProyectoFinal/vista/Panel.java
@@ -39,11 +39,11 @@ public class Panel extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 679, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 526, Short.MAX_VALUE)
         );
 
         pack();
